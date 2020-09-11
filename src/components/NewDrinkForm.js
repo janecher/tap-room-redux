e6d1cd0a41ac3b6926af5a258c79db6dffc5ea94
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-09-11 11:13:49 -0700

    add NewDrinkForm component

---
+++ src/components/NewDrinkForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
+import ReusableForm from "./ReusableForm";
+
+function NewDrinkForm(props){
+
+  function handleNewDrinkFormSubmission(event) {
+    event.preventDefault();
+    props.onNewDrinkCreation({name: event.target.name.value, brand: event.target.brand.value, flavor: event.target.flavor.value, price: event.target.price.value, pints : 124, id: v4()});
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewDrinkFormSubmission}
+        buttonText="Add" />
+    </React.Fragment>
+  );
+}
+
+NewDrinkForm.propTypes = {
+  onNewDrinkCreation: PropTypes.func
+};
+
+export default NewDrinkForm;
