1685897bc01efbeff1129edd72a76678248b3918
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-09-18 22:58:09 -0700

    change styles

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -4,14 +4,16 @@ import PropTypes from "prop-types";
 function Drink(props){
   return (
     <React.Fragment>
-      <div className="panel panel-success">
-        <div className="panel-body">
-          <h3>{props.name}</h3>
-          <p>Brand: {props.brand}</p>
-          <p>Flavor: {props.flavor}</p>
-          <p>Price: <span className = "price">${props.price}</span></p>
-          <p>Pints left: {props.pints}</p>
-          <button onClick={() => props.whenDrinkClicked(props.id)} className="btn btn-secondary">Drink details</button>
+      <div className="col-lg-3 col-md-4 col-sm-6">
+        <div className="panel panel-success">
+          <div className="panel-body">
+            <h3>{props.name}</h3>
+            <p>Brand: {props.brand}</p>
+            <p>Flavor: {props.flavor}</p>
+            <p>Price: <span className = "price">${props.price}</span></p>
+            <p>Pints left: <span className = "price">{props.pints}</span></p>
+            <button onClick={() => props.whenDrinkClicked(props.id)} className="btn btn-secondary">Drink details</button>
+          </div>
         </div>
       </div>
     </React.Fragment>
--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -4,41 +4,45 @@ import PropTypes from "prop-types";
 function ReusableForm(props) {
   return (
     <React.Fragment>
-      <form onSubmit={props.formSubmissionHandler}>
-        <div className ="form-group">
-          <label>Name</label>
-          <input className="form-control" 
-            type='text'
-            name='name'
-            required
-          />
+      <div className="panel panel-success">
+          <div className="panel-body">
+          <form onSubmit={props.formSubmissionHandler}>
+            <div className ="form-group">
+              <label>Name</label>
+              <input className="form-control" 
+                type='text'
+                name='name'
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Brand</label>
+              <input className="form-control"
+                type='text'
+                name='brand'
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Flavor</label>
+              <input className="form-control"
+                type='text'
+                name='flavor'
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Price</label>
+              <input className="form-control"
+                type='text'
+                name='price'
+                required
+              />
+            </div>
+            <button type='submit' className="btn btn-success btn-lg">{props.buttonText}</button>
+          </form>
         </div>
-        <div className="form-group">
-          <label>Brand</label>
-          <input className="form-control"
-            type='text'
-            name='brand'
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Flavor</label>
-          <input className="form-control"
-            type='text'
-            name='flavor'
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Price</label>
-          <input className="form-control"
-            type='text'
-            name='price'
-            required
-          />
-        </div>
-        <button type='submit' className="btn btn-success btn-lg">{props.buttonText}</button>
-      </form>
+      </div>
     </React.Fragment>
   );
 }
